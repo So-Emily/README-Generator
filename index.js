d47dcd6dc66d0721fb5194dce2864026c9efdce0
So-Emily <esoriano110@gmail.com>
2024-06-20 16:50:26 -0700

    Creating a README using its own node program

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     'What is the title of your project?',
     'What is the description of your project?',
-    'What would be in you rtable of contents?',
+    'What would be in your table of contents?',
     'What are the installation instructions?',
     'What is the usage information?',
-    'What are the contribution guidelines',
+    'What are the contribution guidelines?',
     'What are the test instructions?',
     'What license would you like to use?',
     'What is your GitHub username?',
